9d34ee6a3fe2ba4192256dae846d426c0e180dc0
Geonu Choi <6566gun@gmail.com>
2017-05-05 20:00:21 +0900

    Initial Commit

---
+++ TodoFunctionalMVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoFunctionalMVC
+//
+//  Created by Owen Choi on 2017. 5. 5..
+//  Copyright © 2017년 Geonu Choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
